9b41d09e7d926f5b93e8c9b7a25f672cb17b3db3
Eliauk <75741635+crush322@users.noreply.github.com>
2022-03-28 19:35:07 +0800

    Create exercise.py

---
+++ python_biji/02条件语句/exercise.py
@@ -0,0 +1,76 @@
+# print(type (int(50 * 10 / 5)))
+# if 8 > 10:
+#     print("8大于10")
+# else:
+#     print("8不大于10")
+
+# if "wupeiqi" == "alex":
+#     print("相等！")
+# else:
+#     print("不相等！")
+#
+#
+# if int(666) == int(666):
+#     print("相等！")
+# else:
+#     print("不相等！")
+#
+# if str(666) == int(666):
+#     print("相等！")
+# else:
+#     print("不相等！")
+
+# print(30 % 2)
+#
+# a = "我爱我的祖国！"
+# print(3 * a)
+
+# print(int("100")*3) # 300
+# print(int("123") + int("88")) # 211
+# print(str(111) + str(222)) #111222
+# print(str(111) * 3) # 111111111
+# print(int("8") > 7) # True
+# print(str(111) == 111) # False
+# print(type(111)) # int
+# print(bool(-1)) # True
+# print(bool(0)) # False
+# print(bool("")) # False
+# print(bool("你好")) # True
+# print(True == True) # True
+# print(True == False) # False
+# print(bool("") == bool(0)) # True
+# name = "吉诺比利"
+# name0 = "帕克"
+# name_0 = "邓肯"
+# coach = "波波维奇"
+# _= "卡哇伊" # 合法
+# a_year = "1990"
+# year_1 = "1990"
+# _1_year = "1990" # 合法
+# nba_team = "马刺"
+# new_name = "伦纳德"
+# _1 = "maci"
+# print(_)
+# print(_1_year)
+# print(name0)
+# print(_1)
+# 变量不能以数字开头 可以以_开头 也可以仅仅是字母 但不能仅仅是数字
+
+# name = input("请输入用户名：")
+# password = input("请输入密码：")
+# if name == "wupeiqi" and password =="uuu":
+#     print("登录成功！")
+# else:
+#     print("登陆失败！")
+#
+# num = int(input("请随机输入一个数字："))
+# if num > 10:
+#     print("猜错了！")
+# else:
+#     print("猜对了！")
+#
+# num1 = int(input("请输入一个数字："))
+# if num1 % 2 == 0:
+#     print("是偶数呀！")
+# else:
+#     print("是奇数呀！")
