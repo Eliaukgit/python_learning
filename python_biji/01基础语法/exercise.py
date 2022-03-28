7c539137287feeb9149e0cf892a5590ff674be06
Eliauk <75741635+crush322@users.noreply.github.com>
2022-03-28 19:32:35 +0800

    Create exercise.py

---
+++ python_biji/01基础语法/exercise.py
@@ -0,0 +1,67 @@
+print("00")
+
+print("春眠不觉晓,")
+print("处处闻啼鸟,")
+print("夜来风雨声,")
+print("花落知多少.")
+
+
+print(int(50 * 10 / 5))
+if int(8) > int(10):
+    print("8大于10")
+else:
+    print("8不大于10")
+
+print(int(30) % int(2))
+
+a = "我爱我的祖国！"
+b = 3
+print(b * a)
+
+if "wupeiqi" == "alex":
+    print("相等！")
+else:
+    print("不相等！")
+
+
+if int(666) == int(666):
+    print("相等！")
+else:
+    print("不相等！")
+
+if str(666) == int(666):
+    print("相等！")
+else:
+    print("不相等！")
+
+
+print(int("100")*3) # 300
+print(int("123") + int("88")) # 211
+print(str(111) + str(222)) #111222
+print(str(111) * 3) # 111111111
+print(int("8") > 7) # True
+print(str(111) == 111) # False
+print(type(111)) # int
+print(bool(-1)) # True
+print(bool(0)) # False
+print(bool("")) # False
+print(bool("你好")) # True
+print(True == True) # True
+print(True == False) # False
+print(bool("") == bool(0)) # True
+name = "吉诺比利"
+name0 = "帕克"
+name_0 = "邓肯"
+coach = "波波维奇"
+_= "卡哇伊" # 合法
+a_year = "1990"
+year_1 = "1990"
+_1_year = "1990" # 合法
+nba_team = "马刺"
+new_name = "伦纳德"
+_1 = "maci"
+print(_)
+print(_1_year)
+print(name0)
+print(_1)
+# 变量不能以数字开头 可以以_开头 也可以仅仅是字母 但不能仅仅是数字
